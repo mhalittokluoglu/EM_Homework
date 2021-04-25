842f7c2872aafc68b343893007c542ca14c181c9
Muhammed Halit Tokluoğlu <m.halit.tokluoglu@gmail.com>
2021-04-25 12:43:47 +0300

    Fixed Boundary conditions

--- HW2/script1.m
+++ HW2/script1.m
@@ -13,6 +13,7 @@ sigma_2 = 0;
 sigma_3 = 0;
 
 
+
 xcenter = 100;
 E0 = 1;
 Z0 = 120*pi;
@@ -20,7 +21,6 @@ delta_x = 1*10^-3;
 
 c = 3*10^8;
 delta_t = 0.5*delta_x/c;
-% w = 0.001/(2*E0*delta_x);
 f = 10;
 w = 2*pi*f;
 n = 1:512;
@@ -42,6 +42,7 @@ sigma(1:250) = sigma_1;
 sigma(251:309) = sigma_2;
 sigma(310:512) = sigma_3;
 
+
 xp = xcenter*delta_x;
 
 Ey = zeros(1,512);
@@ -52,15 +53,33 @@ for i = 1:512
   Hz(i) = E0/Z0*exp(log(0.001)*((i-xp/delta_x + 1/4)/w)^2);
 end
 
+temp = 0;
 while 1
-  for i = 2:511
-    Hz(i) = Hz(i) - (Ey(i+1)-Ey(i))*delta_t/(delta_x*mu(i));
-    Ey(i) = ep(i)*Ey(i)/(ep(i)+delta_t*sigma(i)) - (Hz(i)-Hz(i-1))*delta_t/(delta_x*(ep(i)+delta_t*sigma(i)));
+  for i = 1:512
+    if i == 1
+      Hz(1) = Hz(i) - (Ey(2)-Ey(1))*delta_t/(delta_x*mu(1));
+      Hz(2) = Hz(i) - (Ey(3)-Ey(2))*delta_t/(delta_x*mu(2));
+      temp = Ey(2);
+      Ey(2) = ep(2)*Ey(2)/(ep(i)+delta_t*sigma(i)) - (Hz(2)-Hz(1))*delta_t/(delta_x*(ep(2)+delta_t*sigma(2)));
+      Ey(1) = temp - (Ey(2) - Ey(1))/3;
+    elseif i == 2
+      continue;
+    elseif i == 511
+      continue;
+    elseif i == 512
+      Hz(511) = Hz(511) - (Ey(512)-Ey(511))*delta_t/(delta_x*mu(511));
+      temp = Ey(511);
+      Ey(511) = ep(511)*Ey(511)/(ep(i)+delta_t*sigma(i)) - (Hz(511)-Hz(510))*delta_t/(delta_x*(ep(511)+delta_t*sigma(511)));
+      Ey(512) = temp - (Ey(511) - Ey(512))/3;    
+    else
+      Hz(i) = Hz(i) - (Ey(i+1)-Ey(i))*delta_t/(delta_x*mu(i));
+      Ey(i) = ep(i)*Ey(i)/(ep(i)+delta_t*sigma(i)) - (Hz(i)-Hz(i-1))*delta_t/(delta_x*(ep(i)+delta_t*sigma(i)));
     end
+  end
   plot(n,Ey);
   line([250 250],[-10 10]);
   line([309 309],[-10 10]);
   ylim([-2 2]);
   xlim([0 512]);
-  pause(0.003);
+  pause(0.01);
 end
